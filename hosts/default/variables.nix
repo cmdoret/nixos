8a37f9a1e200be1bbff4230a0bc2355320648012
cmdoret <cyril.mattheydoret@gmail.com>
2025-06-01 18:44:49 +0200

    refactor: ff as default browser

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -12,7 +12,7 @@
   clock24h = false;
 
   # Program Options
-  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
   keyboardLayout = "us";
   consoleKeyMap = "us";
@@ -28,7 +28,7 @@
   printEnable = false;
 
   # Set Stylix Image
-  stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
+  stylixImage = ../../wallpapers/beautifulmountainscape.jpg;
 
   # Set Waybar
   #  Available Options:
