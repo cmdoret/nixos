3287c99d9a854adb7090526af0ccf1b507c28713
cmdoret <cyril.mattheydoret@gmail.com>
2025-06-01 20:01:48 +0200

    fix: set firefox as default browser

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -5,6 +5,14 @@
     mime.enable = true;
     mimeApps = {
       enable = true;
+      defaultApplications = {
+        "default-web-browser" = ["firefox.desktop"];
+        "text/html" = ["firefox.desktop"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/about" = ["firefox.desktop"];
+        "x-scheme-handler/unknown" = ["firefox.desktop"];
+      };
     };
     portal = {
       enable = true;
