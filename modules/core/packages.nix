bb6e730ebfac5d96f2d36d2627114873e2e357a0
cmdoret <cyril.mattheydoret@gmail.com>
2025-11-09 21:12:53 +0100

    feat(packages): +anki

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -18,6 +18,7 @@
 
   environment.systemPackages = with pkgs; [
     #amfora # Fancy Terminal Browser For Gemini Protocol
+    anki # flashcard and SRS program
     appimage-run # Needed For AppImage Support
     bitwarden-desktop # password manager
     bottom # btop like util
