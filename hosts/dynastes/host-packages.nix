b8b01eb07e3dd65757c963bddb03cdf7ef47a71b
cmdoret <cyril.mattheydoret@gmail.com>
2026-02-15 20:38:21 +0100

    fix(dynastes): freecad wrapper for nvidia+wayland compat

--- hosts/dynastes/host-packages.nix
+++ hosts/dynastes/host-packages.nix
@@ -1,8 +1,23 @@
-{pkgs, ...}: {
+{pkgs, ...}: 
+let
+  # Workaround for freecad not working under nvidia+wayland: force X11
+  freecadWrapped = pkgs.symlinkJoin {
+    name = "FreeCAD";
+    paths = [ pkgs.freecad ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      # Wrap binary
+      wrapProgram $out/bin/FreeCAD \
+        --set QT_QPA_PLATFORM xcb
+
+    '';
+  };
+in
+  {
   environment.systemPackages = with pkgs; [
     audacity
     feishin
-    freecad # computer assisted design
+    freecadWrapped # computer assisted design
     lutris
     obs-studio
     osu-lazer-bin
