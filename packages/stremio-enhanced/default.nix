f479f21c8c32c3a33be5fc1c41e9a58a290b5251
cmdoret <cyril.mattheydoret@gmail.com>
2026-03-25 16:15:57 +0100

    feat(pkg): +stremio-enhanced

--- packages/stremio-enhanced/default.nix
+++ packages/stremio-enhanced/default.nix
@@ -2,74 +2,63 @@
 
 let
   pname = "stremio-enhanced";
-  version = "1.0.0";
+  version = "1.0.2";
 
   system = pkgs.stdenv.hostPlatform.system;
 
-  srcs = {
-    aarch64-darwin = {
-      url = "https://github.com/REVENGE977/${pname}/releases/download/v${version}/mac-arm64.zip";
-      hash = "sha256:342fdcf0ec3c040511edcad538682dec10f94a405d328b3bea43a464298f7003";
-    };
+  # Darwin uses the zip release
+  darwinSrc = pkgs.fetchzip {
+    url = "https://github.com/REVENGE977/${pname}/releases/download/v${version}/mac-arm64.zip";
+    hash = "sha256-m/dL+UIhiK/6ysQi5ihd48qYekD7xqv06UdgNKq8YcI=";
+  };
 
-    x86_64-linux = {
-      url = "https://github.com/REVENGE977/${pname}/releases/download/v${version}/linux-x64.tar.gz";
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  # Linux uses the AppImage release
+  linuxSrc = pkgs.fetchurl {
+    url = "https://github.com/REVENGE977/${pname}/releases/download/v${version}/Stremio.Enhanced-${version}.AppImage";
+    hash = "sha256-rnR+d7eFHAPRVOnpVFIpdWjtNfLjIq1f4uy7prLBHzo=";
+  };
+
+  appimageContents = pkgs.appimageTools.extractType2 {
+    inherit pname version;
+    src = linuxSrc;
+  };
+
+  # Darwin derivation
+  darwinPkg = pkgs.stdenv.mkDerivation {
+    inherit pname version;
+    src = darwinSrc;
+    installPhase = ''
+      mkdir -p $out/Applications
+      cp -r *.app $out/Applications/
+    '';
+    meta = with lib; {
+      description = "Enhanced Stremio client";
+      homepage = "https://github.com/REVENGE977/stremio-enhanced";
+      platforms = [ "aarch64-darwin" "x86_64-darwin" ];
     };
   };
 
-  desktopItem = pkgs.makeDesktopItem {
-    name = pname;
-    exec = pname;
-    icon = pname;
-    desktopName = "Stremio Enhanced";
-    categories = [ "AudioVideo" "Video" ];
+  # Linux derivation
+  linuxPkg = pkgs.appimageTools.wrapType2 {
+    inherit pname version;
+    src = linuxSrc;
+
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/stremio-enhanced.desktop -t $out/share/applications/
+      install -m 444 -D ${appimageContents}/stremio-enhanced.png -t $out/share/pixmaps/
+
+      # Fix the executable name in the desktop file
+      substituteInPlace $out/share/applications/stremio-enhanced.desktop \
+        --replace-fail 'Exec=AppRun' 'Exec=${pname}' \
+        --replace-fail 'Icon=stremio-enhanced' 'Icon=${pname}'
+    '';
+
+    meta = with lib; {
+      description = "Enhanced Stremio client";
+      homepage = "https://github.com/REVENGE977/stremio-enhanced";
+      platforms = [ "x86_64-linux" "aarch64-linux" ];
+    };
   };
 
 in
-pkgs.stdenv.mkDerivation {
-  inherit pname version;
-
-  src = pkgs.fetchzip srcs.${system};
-
-  nativeBuildInputs =
-    [ pkgs.unzip ]
-    ++ lib.optionals pkgs.stdenv.isLinux [
-      pkgs.makeWrapper
-      pkgs.copyDesktopItems
-    ];
-
-  desktopItems = lib.optionals pkgs.stdenv.isLinux [ desktopItem ];
-
-  installPhase = ''
-    mkdir -p $out
-  ''
-  + lib.optionalString pkgs.stdenv.isDarwin ''
-    mkdir -p $out/Applications
-    cp -r *.app $out/Applications/
-  ''
-  + lib.optionalString pkgs.stdenv.isLinux ''
-    # Install app files
-    mkdir -p $out/lib/${pname}
-    cp -r * $out/lib/${pname}/
-
-    chmod +x $out/lib/${pname}/stremio-enhanced
-
-    # Wrapper
-    mkdir -p $out/bin
-    makeWrapper $out/lib/${pname}/stremio-enhanced \
-      $out/bin/${pname} \
-      --prefix LD_LIBRARY_PATH : $out/lib/${pname}
-
-    # Icon (used by desktopItem)
-    if [ -f "$out/lib/${pname}/icon.png" ]; then
-      mkdir -p $out/share/pixmaps
-      cp $out/lib/${pname}/icon.png $out/share/pixmaps/${pname}.png
-    fi
-  '';
-
-  meta = with lib; {
-    description = "Enhanced Stremio client";
-    platforms = builtins.attrNames srcs;
-  };
-}
+if pkgs.stdenv.isDarwin then darwinPkg else linuxPkg
