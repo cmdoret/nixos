af1dd8c3766898f0da059d47350f9e74292eec5a
cmdoret <cyril.mattheydoret@gmail.com>
2026-06-11 01:01:01 +0200

    feat(pkg): +sbx

--- packages/default.nix
+++ packages/default.nix
@@ -1,4 +1,7 @@
 { pkgs, inputs ? {} }:
 {
   stremio-enhanced = pkgs.callPackage ./stremio-enhanced { };
+  docker-sbx = let
+    drv = pkgs.callPackage ./docker-sbx {docker-sbx = drv; };
+  in drv;
 }
