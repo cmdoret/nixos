83736cb8674e2a5508d19a286063f11c6e1cb82d
cmdoret <cyril.mattheydoret@gmail.com>
2026-06-11 02:03:04 +0200

    feat(role): add sbx to development

--- roles/development/default.nix
+++ roles/development/default.nix
@@ -1,7 +1,13 @@
 { pkgs, inputs, lib, config, ... }:
+let
+  docker-sbx = let
+    drv = pkgs.callPackage ../../packages/docker-sbx {docker-sbx = drv; };
+  in drv;
+in 
 {
   config = lib.mkIf config.development.enable {
     environment.systemPackages = with pkgs; [
+      docker-sbx
       kubectl
       kubelogin
       kubelogin-oidc
