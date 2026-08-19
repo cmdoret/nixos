0379f9c037a6a54bfc47b7b7c4092483b0d81749
cmdoret <cyril.mattheydoret@gmail.com>
2026-08-19 16:25:13 +0200

    fix(nvf): deprecated breadcrumbs option

--- modules/home/nvf.nix
+++ modules/home/nvf.nix
@@ -442,10 +442,6 @@ in {
         noice.enable = true;
         colorizer.enable = true;
         illuminate.enable = true;
-        breadcrumbs = {
-          enable = false;
-          navbuddy.enable = false;
-        };
         smartcolumn = {
           enable = true;
         };
