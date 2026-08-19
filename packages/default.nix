f7114b3f8d0b273e63dc0e30d605db73331272c1
cmdoret <cyril.mattheydoret@gmail.com>
2026-08-19 16:55:27 +0200

    chore: drop unused packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,4 @@
 { pkgs, inputs ? {} }:
 {
   stremio-enhanced = pkgs.callPackage ./stremio-enhanced { };
-  docker-sbx = let
-    drv = pkgs.callPackage ./docker-sbx {docker-sbx = drv; };
-  in drv;
 }
